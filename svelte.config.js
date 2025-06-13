ff689b889af644ef12cce0cfb6e6dd5a32685be4
Concerned US Citizen <wipe-bit-6z@icloud.com>
2025-06-12 20:27:43 -0700

    Update production hostname

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,6 @@ import adapter from "@sveltejs/adapter-static";
 
 const dev = process.env.NODE_ENV === "development";
 const base = dev ? "" : (process.env.BASE_PATH ?? "");
-const username = dev ? "" : process.env.GITHUB_USERNAME;
 
 export default {
   kit: {
@@ -13,7 +12,7 @@ export default {
     paths: { base },
 
     prerender: {
-      origin: dev ? "http://localhost:5173" : `https://${username}.github.io`,
+      origin: dev ? "http://localhost:5173" : `https://protestmap.info`,
     },
   },
 };

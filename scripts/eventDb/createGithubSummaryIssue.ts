5dc21742a030e2b1978ff840a412ec5e59c2d00f
Concerned US Citizen <wipe-bit-6z@icloud.com>
2026-01-11 12:38:15 -0800

    Bump event maxRejects until fixed

--- scripts/eventDb/createGithubSummaryIssue.ts
+++ scripts/eventDb/createGithubSummaryIssue.ts
@@ -106,7 +106,7 @@ export async function maybeCreateGithubIssue(summaryInfo: ProcessingSummary) {
   > = {
     event: {
       minRowCount: 10000,
-      maxRejects: 1100,
+      maxRejects: 1137,
     },
     turnout: {
       minRowCount: 1000,
